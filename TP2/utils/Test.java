23d8c385052f4fef42368e5f909e5d9dd3f7a991
mrredcoding <139577689+mrredcoding@users.noreply.github.com>
2023-11-26 19:22:07 +0100

    Update Test.java
    fix UTF8 encoding

--- TP2/utils/Test.java
+++ TP2/utils/Test.java
@@ -16,21 +16,26 @@ public class Test {
     /**
      * Génère une boîte de titre stylisée pour afficher un titre.
      *
+     * Utilise des caractères encodés en UTF-8 pour créer une boîte de titre stylisée.
+     * Les lignes de la boîte sont constituées de caractères de ligne horizontale (─),
+     * de coins (┌, └, ┐, ┘), et d'espaces entourant le titre.
+     *
      * @param title Le titre à afficher dans la boîte.
      * @return Une chaîne représentant la boîte de titre stylisée.
      */
-    public static String titleBox(String title){
-        String repeat = "═".repeat(Math.max(0, title.length() + 2));
+    public static String titleBox(String title) {
+        String repeat = "─".repeat(Math.max(0, title.length() + 2));
 
         return "\u001B[34m" +
                 System.lineSeparator() +
-                "╔" + repeat +  "╗" +
+                "┌" + repeat +  "┐" +
                 System.lineSeparator() +
-                "║ " + title + " ║" +
+                "│ " + title + " │" +
                 System.lineSeparator() +
-                "╚" + repeat +  "╝" +
+                "└" + repeat +  "┘" +
                 "\u001B[0m";
     }
+
 
     /**
      * Affiche et retire le sommet de la pile.
