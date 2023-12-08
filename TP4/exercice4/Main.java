4bb153c02f7cf5c3b48f2ee7547b7ef5b483afe0
cedric.alonso <cedric.alonso.pro@gmail.com>
2023-12-08 09:28:58 +0100

    Improve main

--- TP4/exercice4/Main.java
+++ TP4/exercice4/Main.java
@@ -1,12 +1,20 @@
 package TP4.exercice4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         RecursivIsValidExpression validator = new RecursivIsValidExpression();
 
-        boolean isValid = validator.validate("1+1");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(isValid ? "Yes" : "No");
+        System.out.println("Enter an algebraic expression :");
+        String expression = scanner.nextLine();
+
+        boolean isValid = validator.validate(expression);
+
+        System.out.print(expression + " is ");
+        System.out.println(isValid ? "valid" : "not valid");
     }
 }
