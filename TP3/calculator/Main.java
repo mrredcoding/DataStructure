bf1145ac93a3a36451f79469f01dd4be712c5bb0
mrredcoding <cedric.alonso.pro@gmail.com>
2023-12-10 19:11:49 +0100

    change example

--- TP3/calculator/Main.java
+++ TP3/calculator/Main.java
@@ -19,7 +19,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // Expression infixée à évaluer
-        String infixExpression = "( 2 + 3 ) * ( 6 - ( 7 - 8 ) ) / ( 6 + 1 )";
+        String infixExpression = "( 2 + 3 ) * ( 6 - 2 )";
         InfixToPostfix infixToPostfix = new InfixToPostfix(infixExpression);
 
         // Conversion de l'expression infixée en notation postfixée
