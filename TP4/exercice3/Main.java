d56b4d9d6d5f8f47a66414d14b22ab86b06958b4
mrredcoding <cedric.alonso.pro@gmail.com>
2023-12-10 19:12:01 +0100

    fix display

--- TP4/exercice3/Main.java
+++ TP4/exercice3/Main.java
@@ -2,6 +2,10 @@ package TP4.exercice3;
 
 public class Main {
     public static void main(String[] args) {
-        RecursiveHanoiTower.towers(3, 'A', 'C', 'B');
+        int n = 4;
+
+        System.out.println("To resolve the hanoi towers problem with " + n + " disks," +
+                " you should consider the following steps : ");
+        RecursiveHanoiTower.towers(n, 'A', 'C', 'B');
     }
 }
