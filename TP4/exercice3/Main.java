1701454e7ce74576376b4d8d7dec5ee21d6737c2
Swiiiip <jade.hatoum23@gmail.com>
2023-12-10 20:27:15 +0100

    Recursive TD5 Exo5 fixed

--- TP4/exercice3/Main.java
+++ TP4/exercice3/Main.java
@@ -2,7 +2,7 @@ package TP4.exercice3;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 3;
 
         System.out.println("To resolve the hanoi towers problem with " + n + " disks," +
                 " you should consider the following steps : ");
--- TP4/exercice5/PrefixToPostfix.java
+++ TP4/exercice5/PrefixToPostfix.java
@@ -1,26 +1,40 @@
 package TP4.exercice5;
 import TP2.stack.EmptyStackExceptions;
-import TP2.stack.Stack;
 import TP2.staticStack.StaticStack;
 
 public class PrefixToPostfix {
-    static boolean isOperator(char c) { return c == '+' || c == '-' || c == '*' || c == '/'; }
 
-    static String transform(String prefix) throws EmptyStackExceptions {
-        Stack<String> stack = new StaticStack<>(prefix.length());
-        int length = prefix.length();
+    private static StaticStack<String> stack;
 
-        for (int i = length - 1; i >= 0; i--) {
-            char c = prefix.charAt(i);
+    public static String transform(String prefix) throws EmptyStackExceptions {
+        stack = new StaticStack<>(prefix.length());
+        recursiveTransform(prefix);
+        return stack.pop();
+    }
 
-            if (isOperator(c)) {
-                String operand1 = stack.pop();
-                String operand2 = stack.pop();
-                stack.push(operand1 + operand2 + c);
-            } else
-                stack.push(Character.toString(c));
+    private static String recursiveTransform(String prefix) throws EmptyStackExceptions {
+        if (prefix.isEmpty()) {
+            return "";
         }
 
-        return stack.top();
+        char c = prefix.charAt(0);
+
+        if (isOperator(c)) {
+            String op1 = recursiveTransform(prefix.substring(1));
+            String op2 = recursiveTransform(op1);
+            String operand1 = stack.pop();
+            String operand2 = stack.pop();
+            String newOperand = operand2 + operand1 + c;
+            stack.push(newOperand);
+            return op2;
+        } else {
+            stack.push(Character.toString(c));
+            return prefix.substring(1);
+        }
     }
+
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
 }
