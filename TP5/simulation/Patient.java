709ea098e86fda1d86145e80e0e60f4fc9f06679
Swiiiip <jade.hatoum23@gmail.com>
2023-12-17 22:14:35 +0100

    Added JavaDoc

--- TP5/simulation/Patient.java
+++ TP5/simulation/Patient.java
@@ -1,25 +1,54 @@
 package TP5.simulation;
 
+/**
+ * La classe Patient modélise un patient avec des attributs tels que l'heure d'arrivée et la durée de l'examen.
+ * Chaque patient se voit attribuer un nom unique commençant par "P1".
+ *
+ * @version 1.0
+ * @author Cédric Alonso
+ * @author Jade Hatoum
+ */
 class Patient {
     private static int idCpt = 0;
     private final String name;
     private final int arrivalTime;
     private final int examinationDuration;
 
+    /**
+     * Construit un patient avec l'heure d'arrivée et la durée d'examen spécifiées.
+     *
+     * @param arrivalTime          L'heure à laquelle le patient arrive.
+     * @param examinationDuration La durée de l'examen pour le patient.
+     */
     public Patient(int arrivalTime, int examinationDuration) {
         this.name = "P" + ++idCpt;
         this.arrivalTime = arrivalTime;
         this.examinationDuration = examinationDuration;
     }
 
+    /**
+     * Récupère le nom du patient.
+     *
+     * @return Le nom du patient.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Récupère l'heure d'arrivée du patient.
+     *
+     * @return L'heure d'arrivée du patient.
+     */
     public int getArrivalTime() {
         return arrivalTime;
     }
 
+    /**
+     * Récupère la durée de l'examen du patient.
+     *
+     * @return La durée de l'examen du patient.
+     */
     public int getExaminationDuration() {
         return examinationDuration;
     }
--- TP5/simulation/WaitingRoom.java
+++ TP5/simulation/WaitingRoom.java
@@ -3,19 +3,41 @@ package TP5.simulation;
 import TP5.queue.Queue;
 import TP5.queue.QueueException;
 
-
+/**
+ * La classe WaitingRoom modélise une salle d'attente où les patients sont traités dans l'ordre d'arrivée.
+ * Elle permet d'ajouter des patients à la salle d'attente et de simuler le traitement de ceux-ci.
+ *
+ * @version 1.0
+ * @author Cédric Alonso
+ * @author Jade Hatoum
+ */
 class WaitingRoom {
     public static final int MAX_PATIENTS = 100;
     private final Queue<Patient> waitingRoom;
 
+    /**
+     * Constructeur de la salle d'attente.
+     */
     public WaitingRoom() {
         waitingRoom = new Queue<>(MAX_PATIENTS);
     }
 
+    /**
+     * Ajoute un patient à la salle d'attente.
+     *
+     * @param patient Le patient à ajouter.
+     * @throws QueueException Si la file est pleine.
+     */
     public void addPatient(Patient patient) throws QueueException {
         waitingRoom.push(patient);
     }
 
+    /**
+     * Récupère et retourne le patient arrivant le plus tôt dans la salle d'attente.
+     *
+     * @return Le patient arrivant le plus tôt dans la salle d'attente.
+     * @throws QueueException Si la salle d'attente est vide.
+     */
     public Patient getNextEearliestPatient() throws QueueException {
         Patient currentEarliestPatient = waitingRoom.pop();
         for(int i = 0; i < waitingRoom.getNbElements(); i++){
@@ -30,6 +52,14 @@ class WaitingRoom {
         return currentEarliestPatient;
     }
 
+    /**
+     * Simule le traitement des patients dans la salle d'attente.
+     *
+     * La méthode simule le déroulement du traitement des patients en fonction de leur heure d'arrivée
+     * et de la durée de leur examen.
+     *
+     * @throws QueueException Si la salle d'attente est vide.
+     */
     public void simulate() throws QueueException {
         int totalWaitTime = 0;
         int totalGlobalTime = 0;
@@ -62,7 +92,4 @@ class WaitingRoom {
         System.out.println("\nAverage Global Time: " + averageGlobalTime + " minutes.");
         System.out.println("Average Waiting Time: " + averageWaitTime + " minutes.");
     }
-
-
-
 }
